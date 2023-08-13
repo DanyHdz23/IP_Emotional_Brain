91b12e8ad7609208644d4537d045a592bd57e253
DanyHdz23 <121143771+DanyHdz23@users.noreply.github.com>
2023-08-13 15:20:10 -0600

    face analisis

---
+++ AnalisisESP32.py
@@ -0,0 +1,18 @@
+''' Autor: Daniel Hernández Hernández
+
+Analisis de imagenes capturadas por ESP32 CAM, publicadas en MQTT y visualizacion en Node-Red.
+Las imagenes deben encontrarse en el ordenador y en la misma carpeta.'''
+
+# Librerias
+from deepface import DeepFace
+import paho.mqtt.client as mqtt
+
+# Conexión a mqtt
+client = mqtt.Client()
+client.connect("IP",port,60)
+
+# Analisis de la captura
+emotion = DeepFace.analyze(img_path='direccion de la imagen: home/captura, etc', actions=['emotion','age','gender'])
+parametro = emotion[0]['dominant_emotion']
+client.publish("Topic a publicar", parametro)
+print(f"El resultado es: {parametro}")
